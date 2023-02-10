07e6e3298384fafb7aa35b66208039b3951f5e99
taras.chmeruk <taras.chmeruk@gameloft.com>
2023-02-10 08:55:01 +0200

    first commit

---
+++ src/test/java/com/spring/microservices/demo/configservergit/ConfigserverGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.demo.configservergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
